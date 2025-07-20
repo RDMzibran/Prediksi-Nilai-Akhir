50c2837452c001baa957596c08a294fc1abc358d
RDMzibran <zibranwaliulu64@gmail.com>
2025-07-20 19:35:16 +0900

    COMMIT CHANGE

--- app.py
+++ app.py
@@ -86,9 +86,12 @@ def prediksi():
 
 # Otomatis buka browser
 def open_browser():
-    webbrowser.open_new("http://127.0.0.1:5000")
+    import socket
+    hostname = socket.gethostname()
+    ip_local = socket.gethostbyname(hostname)
+    webbrowser.open_new(f"http://{ip_local}:5000")
 
 # Run Flask
 if __name__ == "__main__":
     threading.Timer(1.0, open_browser).start()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- script.js
+++ script.js
@@ -17,7 +17,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const data = { tugas, uts, uas, kehadiran };
 
-        fetch("http://127.0.0.1:5000/prediksi", {
+        fetch("http://192.168.1.42:5000/prediksi", {
+
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data)
